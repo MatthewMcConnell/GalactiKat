14a7de39558fd2c2a148fe2222558bcf66f63b2a
Anna Savova <2327851s@student.gla.ac.uk>
2020-10-03 19:37:00 +0100

    added Whirpool prefab and script

---
+++ meowstronaut/Assets/Scripts/GravityWhirpool.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWhirpool : MonoBehaviour
+{
+    public bool inRadius = false;
+
+    public Rigidbody2D Radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // if hit by Kat, restart level
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("GalactiKat collided with whirpool");
+            LevelController.RestartLevel();
+        }
+    }
+}
