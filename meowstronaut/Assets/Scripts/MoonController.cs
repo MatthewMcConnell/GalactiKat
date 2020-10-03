7f6b4a3085e1100e4f88e9810bb2faca996f9744
Matthew McConnell <matthewmcconnell04@gmail.com>
2020-10-03 09:02:43 +0100

    Added Background and Moon
    Created the simple scenes for us to work and test in. I created prefabs of the background and simple moon initially. You should be able to use these in any scene and they will work as they did for me :)

---
+++ meowstronaut/Assets/Scripts/MoonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonController : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public Rigidbody2D moonRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moonRigidBody.AddTorque(rotationSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
